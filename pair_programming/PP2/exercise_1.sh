1c9cae68e907984f39cabe9998f2931727cf80cb
laurynwu0125 <laurynwu@college.harvard.edu>
2021-09-16 11:24:37 -0400

    testing bash script

---
+++ pair_programming/PP2/exercise_1.sh
@@ -0,0 +1,22 @@
+#! usr/bin/env bash
+read -r -p "What file do you want to commit? " filename
+git add $filename
+git status
+read -n1 -p "Do you want to continue? (Y or N) " continue
+if [[ $continue == "Y" ]]
+then 
+	read -p "What is the commit message? " message
+	git commit -m "$message"
+	git status
+	read -n1 -p "Do you want to continue? (Y or N) " continue2
+	if [[ $continue2 == "Y" ]]
+	then
+		git push
+	else
+		exit 1
+	fi
+else
+	exit 1
+fi
+done 
+ 
