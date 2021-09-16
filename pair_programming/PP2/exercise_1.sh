83216dcbebf9f73071f53926669c6c0f057600d6
laurynwu0125 <laurynwu@college.harvard.edu>
2021-09-16 11:31:48 -0400

    testing bash 2

--- pair_programming/PP2/exercise_1.sh
+++ pair_programming/PP2/exercise_1.sh
@@ -1,14 +1,14 @@
 #! usr/bin/env bash
-read -r -p "What file do you want to commit? " filename
+read -r -p $'What file do you want to commit?\n' filename
 git add $filename
 git status
-read -n1 -p "Do you want to continue? (Y or N) " continue
+read -n1 -p $'Do you want to continue? (Y or N)\n' continue
 if [[ $continue == "Y" ]]
 then 
-	read -p "\nWhat is the commit message? " message
+	read -p $'What is the commit message?\n' message
 	git commit -m "$message"
 	git status
-	read -n1 -p "Do you want to continue? (Y or N) " continue2
+	read -n1 -p $'Do you want to continue? (Y or N)\n' continue2
 	if [[ $continue2 == "Y" ]]
 	then
 		git push
